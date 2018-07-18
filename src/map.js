55f2af3c812f6e184efbf9c0bf3e0d629eb13138
Nikolai <nikolai.laevskii@yandex.ru>
2018-07-18 15:47:33 +0300

    Кластеры теперь правильно окрашиваются

--- src/map.js
+++ src/map.js
@@ -31,15 +31,35 @@ export function initMap(ymaps, containerId) {
     geoObjectBalloonContentLayout: getDetailsContentLayout(ymaps)
   });
 
+  /**
+   * По умолчанию всем кластерам ставится зеленый цвет
+   */
   objectManager.clusters.options.set('preset', 'islands#greenClusterIcons');
-  
+  /**
+   * Когда происходит создание кластера, проверяются все объекты в нем.
+   * Если в нем есть неактивные объекты (дефектные станции), то делаем кластер оранжевым.
+   * Если все станции в кластере дефектны, то делаем кластер красным.
+   * Доки: https://tech.yandex.ru/maps/doc/jsapi/2.1/ref/reference/objectManager.ClusterCollection-docpage
+   */
+  objectManager.clusters.events.add('add', function (e) {
+    const cluster = objectManager.clusters.getById(e.get('objectId')); //узнаем что у нас за кластер
+    const objects = cluster.properties.geoObjects; //получаем все объекты кластера
+    const inactiveObjsCount = objects.reduce((ac, cv) => //итерируем через все объекты, одновременно считая их
+      cv.isActive ? ac //если объект активен, то ничего не делаем
+      : objectManager.clusters.setClusterOptions(cluster.id, {preset:'islands#orangeClusterIcons'}) && ac+1, 0 //если нет, то делаем кластер оранжевым и считаем его
+    )
+    if (objects.length == inactiveObjsCount) { //если количество неактивных объектов = количеству объектов в кластере
+      objectManager.clusters.setClusterOptions(cluster.id, {preset:'islands#redClusterIcons'}) //делаем кластер красным
+    }
+  })
+
   /**
    * Тут производится добавление объектов в objectManager 
    * [1] Пример: https://tech.yandex.ru/maps/doc/jsapi/2.1/ref/reference/ObjectManager-docpage/#method_detail__add
    */
   loadList().then(data => {
     log('adding data to objectManager', 'initMap', 'info', data)
-    objectManager.add(data); 
+    objectManager.add(data);
     myMap.geoObjects.add(objectManager); //Для добавления объектов на карту тут не хватало этой строчки [1]
   });
 
