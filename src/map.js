c2ba71bbb0cf8bd94febfcd0d9ad823d43e04b56
Nikolai <nikolai.laevskii@yandex.ru>
2018-07-19 04:55:03 +0300

    Более разумная покраска кластеров

--- src/map.js
+++ src/map.js
@@ -31,10 +31,6 @@ export function initMap(ymaps, containerId) {
   });
 
   /**
-   * По умолчанию всем кластерам ставится зеленый цвет
-   */
-  objectManager.clusters.options.set('preset', 'islands#greenClusterIcons');
-  /**
    * Когда происходит создание кластера, проверяются все объекты в нем.
    * Если в нем есть неактивные объекты (дефектные станции), то делаем кластер оранжевым.
    * Если все станции в кластере дефектны, то делаем кластер красным.
@@ -43,14 +39,16 @@ export function initMap(ymaps, containerId) {
   objectManager.clusters.events.add('add', function (e) {
     const cluster = objectManager.clusters.getById(e.get('objectId')); //узнаем что у нас за кластер
     const objects = cluster.properties.geoObjects; //получаем все объекты кластера
+    let preset = 'islands#greenClusterIcons'; //ставим по умолчанию зеленый цвет
     const inactiveObjsCount = objects.reduce((ac, cv) => //итерируем через все объекты, одновременно считая их
-      cv.isActive ? ac //если объект активен, то ничего не делаем
-      : objectManager.clusters.setClusterOptions(cluster.id, {preset:'islands#orangeClusterIcons'}) && ac+1, 0 //если нет, то делаем кластер оранжевым и считаем его
+      cv.isActive ? ac : ~(preset = 'islands#orangeClusterIcons') && ac+1, 0 //если объект дефективен, то ставим оранжевый цвет и считаем его
     )
     if (objects.length == inactiveObjsCount) { //если количество неактивных объектов = количеству объектов в кластере
-      objectManager.clusters.setClusterOptions(cluster.id, {preset:'islands#redClusterIcons'}) //делаем кластер красным
+      //делаем кластер красным
+      preset = 'islands#redClusterIcons';
     }
-  })
+    objectManager.clusters.setClusterOptions(cluster.id, { preset }); //выставляем цвет кластера
+  });
 
   /**
    * Тут производится добавление объектов в objectManager 
